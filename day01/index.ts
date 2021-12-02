35c402dc66a0f47b8b007f64d699e4463f193502
Mark van Straten <mark.van.straten@nn.nl>
2021-12-02 11:09:13 +0100

    cleanup

--- day01/index.ts
+++ day01/index.ts
@@ -3,46 +3,54 @@ import { promisify } from "util";
 
 const readFilePromise = promisify(readFile);
 
-readFilePromise("./input.txt", "utf8")
-  .then((content) => content.split("\n"))
-  .then((lines) => lines.map((line) => parseInt(line, 10)))
+parseReadingsFromFile("./input.txt")
   .then((sonarDepthReadings) => countIncreases(sonarDepthReadings))
   .then((increases) => console.log(`Part one - Total increases: ${increases}`));
 
-
 // part two, reduce the input to triplets, sum them together
-readFilePromise("./input.txt", "utf8")
-  .then((content) => content.split("\n"))
-  .then((lines) => lines.map((line) => parseInt(line, 10)))
-  .then((sonarDepthReadings) => sonarDepthReadings
-    .map((_, currentIndex) => {
-        const currentValue = sonarDepthReadings[currentIndex];
-        const secondValue = sonarDepthReadings[currentIndex + 1];
-        const thirdValue = sonarDepthReadings[currentIndex + 2];
-        if (secondValue === undefined || thirdValue === undefined) { 
-            return undefined; 
-        }
-        return currentValue + secondValue + thirdValue;
-    })
-    .filter((slidingWindowValue => slidingWindowValue != undefined)) as Array<number>
-  )
-  .then(sonarDepthReadings => countIncreases(sonarDepthReadings))
+parseReadingsFromFile("./input.txt")
+  .then((sonarDepthReadings) => mapReadingsToTriplets(sonarDepthReadings))
+  .then((sonarDepthReadings) => countIncreases(sonarDepthReadings))
   .then((increases) => console.log(`Part Two - Total increases: ${increases}`));
 
-function countIncreases(sonarDepthReadings: Array<number>): number {
-    return sonarDepthReadings.reduce(
-        (accumulator, currentReading) => {
-            if (accumulator.previousReading && currentReading > accumulator.previousReading ) {
-            accumulator.increases++;
-            }
+function parseReadingsFromFile(file: string): Promise<Array<number>> {
+  return readFilePromise("./input.txt", "utf8")
+    .then((content) => content.split("\n"))
+    .then((lines) => lines.map((line) => parseInt(line, 10)));
+}
 
-            accumulator.previousReading = currentReading;
-            return accumulator;
-        },
-        { previousReading: undefined, increases: 0 } as {
-            previousReading?: number;
-            increases: number;
-        }
-    )
-    .increases;
+function countIncreases(sonarDepthReadings: Array<number>): number {
+  return sonarDepthReadings.reduce(
+    (accumulator, currentReading) => {
+      if (
+        accumulator.previousReading &&
+        currentReading > accumulator.previousReading
+      ) {
+        accumulator.increases++;
+      }
+
+      accumulator.previousReading = currentReading;
+      return accumulator;
+    },
+    { previousReading: undefined, increases: 0 } as {
+      previousReading?: number;
+      increases: number;
+    }
+  ).increases;
+}
+
+function mapReadingsToTriplets(readings: Array<number>): Array<number> {
+  return readings
+    .map((_, currentIndex) => {
+      const currentValue = readings[currentIndex];
+      const secondValue = readings[currentIndex + 1];
+      const thirdValue = readings[currentIndex + 2];
+      if (secondValue === undefined || thirdValue === undefined) {
+        return undefined;
+      }
+      return currentValue + secondValue + thirdValue;
+    })
+    .filter(
+      (slidingWindowValue) => slidingWindowValue != undefined
+    ) as Array<number>;
 }
